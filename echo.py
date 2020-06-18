f63ac4ba5584f4b59c80821251e669d000eaeebb
Leanne Benson <stylistleannerowe@gmail.com>
2020-06-17 21:06:50 -0400

    messageparser

--- echo.py
+++ echo.py
@@ -6,18 +6,31 @@ __author__ = "LeanneBenson"
 
 
 import sys
-
+import argparse
 
 def create_parser():
-    """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
-
-
+    parser = argparse.ArgumentParser(
+        description="Echo and Transform a message")
+    parser.add_argument('msg', help="Message to display.")
+    parser.add_argument('-u', "--upper", action='store_true',
+                        help="Transfors the message to Uppercase.")
+    parser.add_argument('-l', '--lower',  action='store_true',
+                        help="Transforms the message to lowercase.")
+    parser.add_argument('-t', '--title', action='store_true',
+                        help="Transforms the message to Title Case.")
+    return parser
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    ns = parser.parse_args()
+    msg = ns.msg
+    if ns.upper:
+        msg = msg.upper()
+    if ns.lower:
+        msg = msg.lower()
+    if ns.title:
+        msg = msg.title()
+    print(msg)
 
 
 if __name__ == '__main__':
